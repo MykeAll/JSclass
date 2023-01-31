35eafe2c82526060e75a8de6661df5970ded55bf
unknown <maremcoGodbless55@gmail.com>
2023-01-31 12:03:15 +0100

    Pig Game commit

--- DOMapp.js
+++ DOMapp.js
@@ -14,18 +14,119 @@ var score, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
+/*
 dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice);
+*/
 
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// MDN website for event
+
+/*
+function btn() {
+    //Do something here
+}
+btn();
+*/
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1.   Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //  2.  Display the result
+        
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //  3.  Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        // roundScore = roundScore + dice;
+    } else {        
+        //Next player
+        nextPlayer();
+
+
+        /*
+        activePlayer === 0 ? activePlayer = 1   : activePlayer = 0;
+        roundScore = 0;
+        
+        // if(activePlayer === 0) {
+        //     activePlayer = 1;
+        // } else {
+        //     activePlayer = 0;
+        // }
+
+        document.getElementById('current--0').textContent = '0';
+        document.getElementById('current--1').textContent = '0';
+
+        document.querySelector('.player--0-panel').classList.toggle('active');
+        document.querySelector('.player--1-panel').classList.toggle('active');
+
+        // document.querySelector('player--0-panel').classList.remove('active');
+        // document.querySelector('player--1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+        */
+    }
+});
+
+/*
 document.querySelector('#current--' + activePlayer).textContent = dice;
 // document.querySelector('#current--' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 var x = document.querySelector('#score--0').textContent;
 console.log(x);
+*/
 
-document.querySelector('.dice').style.display = 'none';
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+    // scores[activePlayer] = scores[activePlayer] + roundScore;  SAME
 
-// MDN website for event
-document.querySelector('.btn--roll').addEventListener('click',)
+    //Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    //Check if  player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+document.querySelector('.player-' + activePlayer + '-panel').classList.add('player-winner');
+document.querySelector('.player-' + activePlayer + '-panel').classList.remove('player-active');
+    } else {
+        //Next player
+        nextPlayer();
+    }
+    
+
+});
+
+function nextPlayer () {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1   : activePlayer = 0;
+    roundScore = 0;
+    
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('player--0-panel').classList.remove('active');
+    // document.querySelector('player--1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
